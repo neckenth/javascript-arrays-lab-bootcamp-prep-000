6fa8e72476c9a07fdf469fcadd606581216fe525
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2017-09-09 20:14:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much."
+var kittens = [
+    'kitten1',
+    'kitten2',
+    'kitten3'
+]
+function destructivelyAppendKitten(a, name) {
+    a = kittens
+    a.push(name)
+}
